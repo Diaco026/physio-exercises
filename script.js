674ba5101429057ec7d0ba776e44e069e15c3738
Diaco026 <ahmad75.vaziri@gmail.com>
2026-09-02 22:50:28 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function showBodyParts(level){
-  document.getElementById('levelsPage').classList.add('hidden');
+function chooseLevel(level){
+  document.getElementById('levelPage').classList.add('hidden');
   document.getElementById('bodyPage').classList.remove('hidden');
-  document.getElementById('selectedLevel').textContent = 'سطح انتخاب‌شده: ' + level;
-  window.scrollTo({top:0, behavior:'smooth'});
+  document.getElementById('levelBadge').textContent = 'سطح انتخاب‌شده: ' + level;
+  window.scrollTo({top:0,behavior:'smooth'});
 }
 function backToLevels(){
   document.getElementById('bodyPage').classList.add('hidden');
-  document.getElementById('levelsPage').classList.remove('hidden');
-  window.scrollTo({top:0, behavior:'smooth'});
+  document.getElementById('levelPage').classList.remove('hidden');
+  window.scrollTo({top:0,behavior:'smooth'});
 }
